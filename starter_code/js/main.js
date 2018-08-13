5af6b67d43c7dc2fc2646eab2dbd82039bfc1bda
ttjahjadi93 <thomasTjahjadi93@gmail.com>
2018-08-13 14:42:09 +1000

    Initial commit

---
+++ starter_code/js/main.js
@@ -0,0 +1,37 @@
+// document.querySelector('a').addEventListener('click', function() {
+//     event.preventDefault()
+
+// })
+
+
+// document.querySelector('.readmore a').addEventListener("click" , function() {
+//     event.preventDefault()
+//     // you can write here
+// })
+
+var readmore = true
+
+// This is to prevent from any a tag to scroll up when its running a function
+$('.readmore a, .readless a, .learnmore').click(function(){
+    event.preventDefault()
+
+})
+
+$('.readmore a').click(function() {
+    $('#show-this-on-click').slideDown(500)
+    $('.readless').removeClass('hide')
+    $('.readmore').hide()
+})
+
+$('.readless a').click(function() {
+    $('#show-this-on-click').slideUp(500)
+    $('.readless').addClass('hide')
+    $('.readmore').show()
+})
+
+
+$('.learnmore').click(function(){
+    $('#learnmoretext').slideDown()
+    $('.learnmore').hide()
+
+})
